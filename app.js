514aaf7b029be4758cd65727730d0abe18b5b99b
Nicholas Chug <nicholas.chug@gmail.com>
2019-08-23 12:10:39 -0400

    further styling completed and partials added

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ $(document).ready(function() {
 
     let $countDownDate;
 
+    const differenceCalculator = function (differenceInTime) {
+        if (differenceInTime > 0) {
+            let days = Math.floor(differenceInTime / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((differenceInTime % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((differenceInTime % (1000 * 60)) / 1000);
+            $(`.timeUntil`).html(`<p>The time until is: ${days}d, ${hours}h, ${minutes}m, ${seconds}s</p>`);
+        } else {
+            $(`.timeUntil`).html(`<p>The countdown is over!</p>`);
+            $(`.timerOn`).toggleClass(`timerOff`);
+        };
+    };
+
     $('.firstForm').on('submit', function(e) {
         e.preventDefault();
+
         $countDownDate = new Date($(`#dateText`).val()).getTime();
+
+        $(`.dateText`).val('');
+
         const timer = setInterval(function () {
 
             let $timerTitle = $('#titleText').val();
@@ -23,17 +40,5 @@ $(document).ready(function() {
             differenceCalculator(difference);
 
         }, 1000);
-
-        const differenceCalculator = function(differenceInTime) {
-            if (differenceInTime > 0) {
-                let days = Math.floor(differenceInTime / (1000 * 60 * 60 * 24));
-                let hours = Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                let minutes = Math.floor((differenceInTime % (1000 * 60 * 60)) / (1000 * 60));
-                let seconds = Math.floor((differenceInTime % (1000 * 60)) / 1000);
-                $(`.timeUntil`).html(`<p>The time until is: ${days}d, ${hours}h, ${minutes}m, ${seconds}s</p>`);
-            } else {
-                $(`.timeUntil`).html(`<p>The countdown is over!</p>`);
-            };
-        };
     });
 });
