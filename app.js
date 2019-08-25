f9960c36fe762a862fbf47448298c263285da832
Nicholas Chug <nicholas.chug@gmail.com>
2019-08-25 14:34:39 -0400

    commenting and further responsiveness added

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// TO DO
-// Do stretch goals
-// Maybe: organize into cntdwnApp object
+// HackerYou Project #3 - Nicholas Chug
 
-const cntdwnApp = {};
-
+// Document ready function 
 $(document).ready(function() {
 
+    // Global variable countDownDate created which is to be used in the event listener
     let $countDownDate;
 
+    // differenceCalculator function created to calculate the difference in time between a user created date and the present date upon form submission
     const differenceCalculator = function (createdDate, differenceInTime) {
         if (isNaN(createdDate)) {
             $(`.timeUntil`).html(`<p>Please enter a correct date!</p>`);
@@ -23,15 +22,20 @@ $(document).ready(function() {
         };
     };
 
+    // Even listener attached to the first form, which will be called upon submit
     $('.firstForm').on('submit', function(e) {
         e.preventDefault();
 
+        // Previously created Global Variable now called to store today's date and time
         $countDownDate = new Date($(`#dateText`).val()).getTime();
 
+        // Upon form submission, date text is emptied for accessibility
         $(`.dateText`).val('');
 
+        // Timer off button created dynamically 
         $(`.offButtonArea`).html(`<input type='submit' class='offButton' value='Stop Timer'>`);
 
+        // Interval timer attached to the users created date runs function differenceCalculator every second to display an active timer countdown
         const timer = setInterval(function () {
 
             let $timerTitle = $('#titleText').val();
@@ -45,6 +49,7 @@ $(document).ready(function() {
 
         }, 1000);
 
+        // Off button event listener used to stop the timer countdown upon users click
         $('.offButton').on('click', function () {
             clearInterval(timer);
         })
